ef002d18b989b7bb5970d18554ab463fb5dcd415
Raemix <mrmixon82@gmail.com>
2020-05-03 10:05:30 -0500

    Window Class

---
+++ Sparky-core/main.cpp
@@ -0,0 +1,28 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "src\graphics\window.h"
+
+int main()
+{
+	using namespace sparky;
+	using namespace graphics;
+
+	Window window("Sparky", 1200, 675);
+
+
+	while (!window.closed())
+	{
+		std::cout << window.getWidth() << "x" << window.getHeight() << std::endl;
+		window.clear();
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glVertex2f(-0.5f, 0.5f);
+		glEnd();
+		window.update();
+	}
+
+
+
+	return 0;
+}
---
+++ Sparky-core/src/graphics/window.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+namespace sparky {
+	namespace graphics
+	{
+		class Window
+		{
+
+		private:
+			int m_Width, m_Height;
+			const char* m_Title;
+			GLFWwindow* m_Window;
+			bool m_Closed;
+
+
+		public:
+			Window(const char* title, int width, int height);
+			~Window();
+
+			void update();
+			bool closed() const;
+			void clear() const;
+			inline int getWidth() const { return m_Width; }
+			inline int getHeight() const { return m_Height; }
+			void windowResize(GLFWwindow* window, int width, int height);
+
+
+		private:
+			bool init();
+
+
+		};
+
+
+
+
+	}
+}
